2e833ddb1b5a675afd1f53ed9267012d3e6a78d3
Tawanna Hakkinen <thol_16@hotmail.com>
2023-06-30 20:52:49 -0300

    Fix response type of the POST method

--- src/main/java/com/hakkinenT/desafio3/controllers/ClientController.java
+++ src/main/java/com/hakkinenT/desafio3/controllers/ClientController.java
@@ -3,10 +3,14 @@ package com.hakkinenT.desafio3.controllers;
 import com.hakkinenT.desafio3.dto.ClientDTO;
 import com.hakkinenT.desafio3.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -15,7 +19,9 @@ public class ClientController {
     private ClientService service;
 
     @PostMapping
-    public ClientDTO insert(@RequestBody ClientDTO dto){
-        return service.insert(dto);
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+        ClientDTO response = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(response.getId()).toUri();
+        return ResponseEntity.created(uri).body(response);
     }
 }
