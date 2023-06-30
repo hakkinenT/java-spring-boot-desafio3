b2aeeb7c918284c26f139512d5194aa2d0e05d8b
Tawanna Hakkinen <thol_16@hotmail.com>
2023-06-30 17:14:26 -0300

    Database seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('José Santos', '61031827005', 4000.0, '1987-05-10', 5);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Maria Silva', '35355389040', 6000.0, '1967-08-12', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Teresa Oliveira', '51170457002', 8000.0, '1982-11-20', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Felipe Cruz', '65098151090', 4500.0, '1982-09-23', 4);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('João Santos', '31905159056', 4800.0, '1978-12-25', 6);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Cláudia Fernandes', '59784496003', 5500.0, '1990-09-09', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Juliana Ferreira', '84479327029', 5700.0, '1988-02-02', 4);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Auxiliadora Araújo', '76689987051', 4000.0, '1985-11-10', 5);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Carlos Melo', '71775877051', 7000.0, '1970-03-16', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Pedro Gonçalves', '98831188054', 8900.0, '1980-07-24', 3);
